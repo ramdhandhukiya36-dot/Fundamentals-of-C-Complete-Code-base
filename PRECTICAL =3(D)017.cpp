277560a4229746bc1e8a0464d4fd65081db304be
ramdhandhukiya36-dot <ramdhandhukiya36@gmail.com>
2026-08-24 08:38:48 +0530

    Implicit & explicit type conversion

---
+++ PRECTICAL =3(D)017.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int num1=10,num2=3;
+	
+	float result1;
+	
+	result1 = num1/num2;
+	printf("implicit conversion =%.2f\n",result1);
+	
+	float result2;
+	
+	result2=(float)num1/num2;
+	printf("explicit conversion =%.2f\n",result2);
+	
+	return 0;
+}
